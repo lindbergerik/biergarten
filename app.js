4cc1a71d36e641b90e38275c325371ea370eab71
sblomk <sebbeblomkvist@gmail.com>
2018-05-15 15:03:55 +0200

    shits

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ document.addEventListener('prechange', function(event) {
   document.querySelector('ons-toolbar .center')
     .innerHTML = event.tabItem.getAttribute('label');
 });
+
+window.fn = {};
+
+window.fn.open = function() {
+  var menu = document.getElementById('menu');
+  menu.open();
+};
+
+window.fn.load = function(page) {
+  var content = document.getElementById('content');
+  var menu = document.getElementById('menu');
+  content.load(page)
+    .then(menu.close.bind(menu));
+};
